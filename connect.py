b61ce996599942c42d3f27138be1ba007852c103
The_TecnoKing <75082274+TheTecnoKing@users.noreply.github.com>
2022-02-25 22:48:15 +0100

    Realizzato prototipo per la connettività multipla
    Aggiunto il multithreading a connect.py come esperimento per il supporto alle connessioni BT simultanee. È ancora WIP, e dev'essere meglio testata.
    Implementata anche la libreria logging, per poter avere un sistema di log fatto come si deve (e non print() a destra e manca).

--- connect.py
+++ connect.py
@@ -1,46 +1,68 @@
 # al momento non ho modo di testare la connettività multipla tra dispositivi
-# inoltre non è neanche predisposto per più connessioni per ora
 
 import bluetooth
 import json
+import concurrent.futures
+import logging
+import uuid
 
 with open("config.json", mode="r") as config:
     settings = json.load(config)
     config.close()
 
-ssock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-ssock.bind(("", bluetooth.PORT_ANY))
-ssock.listen(1)
-uuid = settings["uuid"]
+def connectDevices(threadID: int, uuid: str):
+    ssock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    ssock.bind(("", bluetooth.PORT_ANY))
+    ssock.listen(1)
+    port = ssock.getsockname()[1]
 
-bluetooth.advertise_service(
-    sock=ssock,
-    name="RoboTranslator",
-    service_id=uuid,
-    service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
-    profiles=[bluetooth.SERIAL_PORT_PROFILE]
-)
-print("Servizio Bluetooth attivo! In attesa di connessione dai dispositivi...")
+    bluetooth.advertise_service(
+        sock=ssock,
+        name="RoboTranslator",
+        service_id=uuid,
+        service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
+        profiles=[bluetooth.SERIAL_PORT_PROFILE]
+    )
+    logging.info("(THREAD %s) Servizio Bluetooth attivo! In ascolto nel canale: %s", threadID, port)
+    logging.debug("(THREAD %s) UUID: %s", threadID, uuid)
 
-csock, cinfo = ssock.accept()
-if cinfo[0] in settings["devices"].keys():
-    print("Dispositivo connesso!", cinfo[0] + " - " + settings["devices"].get(cinfo[0]))
-else:
+    csock, cinfo = ssock.accept()
+    if cinfo[0] in settings["devices"].keys():
+        logging.info("(THREAD %s) Dispositivo connesso! %s - %s", threadID, cinfo[0], settings["devices"].get(cinfo[0]))
+    else:
+        csock.close()
+        logging.warning("(THREAD %s) Tentativo di connessione da parte di %s negato! Dispositivo non riconosciuto!", threadID, cinfo[0])
+
+    # da qui inizia il copiaticcio per gli esperimenti, nulla di definitivo da qui in poi
+    try:
+        while True:
+            data = csock.recv(1024)
+            if not data:
+                break
+            print("Received", data)
+    except OSError:
+        pass
+
+    print("Disconnected.")
+
     csock.close()
-    print("Tentativo di connessione da parte di {0} negato! Dispositivo non riconosciuto!".format(cinfo[0]))
+    ssock.close()
+    print("All done.")
+    # fine copiaticcio
 
-# da qui inizia il copiaticcio per gli esperimenti, nulla di definitivo da qui in poi
-try:
-    while True:
-        data = csock.recv(1024)
-        if not data:
-            break
-        print("Received", data)
-except OSError:
-    pass
+# un giorno questo andrà dritto dritto nel main.py
+if __name__ == "__main__":
+    format = "%(asctime)s | [%(levelname)s] %(message)s"
+    # il livello di log verrà definito tramite parametro CLI in futuro
+    logging.basicConfig(format=format, level=logging.DEBUG, datefmt="%H:%M:%S")
 
-print("Disconnected.")
+    # questa variabile dovrà essere ricavata dal n. di dispositivi nel JSON
+    devices = 5
 
-csock.close()
-ssock.close()
-print("All done.")
+    # devo vedere se uuid diversi servano effettivamente o posso usarne sempre uno e basta
+    uuids = list()
+    for i in range(devices):
+        uuids.append(str(uuid.uuid4()))
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=devices) as executor:
+        executor.map(connectDevices, range(devices), uuids)
